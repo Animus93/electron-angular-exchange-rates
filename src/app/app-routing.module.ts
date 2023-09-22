415681e1e00ccf88b49bc0a7597425455f010e5d
Animus93 <unitofpeople@gmail.com>
2023-09-22 15:57:54 +0300

    upd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './pages/main/main.component';
+import { AboutComponent } from './pages/about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'main',
+    component: MainComponent
+  },
+  {
+    path: 'main/about',
+    component: AboutComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'main',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+}
